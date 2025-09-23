f09d036e12118b037e3a44f9f4246a722654209e
Mauricio Ocando <mauricio.ocando@moveapps.cl>
2025-09-23 02:27:21 -0300

    test(mocks): add alt prop to NextImage mock for better accessibility in tests

--- src/__mocks__/nextImageMock.tsx
+++ src/__mocks__/nextImageMock.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 
-const NextImageMock = (props: any) => <img {...props} />;
+const NextImageMock = (props: any) => <img {...props} alt={props.alt || ""} />;
 export default NextImageMock;
